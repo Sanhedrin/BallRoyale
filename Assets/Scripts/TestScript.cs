37f870311c7a861519467cad74034d23650ff8ff
Paximillian <morzil@walla.com>
2015-07-09 18:39:20 +0300

    Fixing the stupid testscript

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,21 +5,14 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
 		int j;
 		//wuzzzzzup
-=======
 	//דימה לא חבר
->>>>>>> origin/master
 	}
 	
 	// Update is called once per frame
 	void Update () {
-<<<<<<< HEAD
 	//hello world!
 		//EITAN lets play hots!!!
-=======
-		
->>>>>>> origin/master
 	}
 }
