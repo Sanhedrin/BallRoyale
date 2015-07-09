d9d439c5845a73d1c0148d31a8e1b84ba66f17a2
Gezer <guydunski@gmail.com>
2015-07-09 18:06:07 +0300

    i chaned stuff
    about what we need to do

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,11 +5,13 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		int j;
+		//wuzzzzzup
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	//hello world!
+		//EITAN lets play hots!!!
 	}
 }
