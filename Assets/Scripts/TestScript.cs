4658b435a2121b3f8b4012acb8f1974a7a156293
eitanacton <eitanacton@gmail.com>
2015-07-09 18:06:07 +0300

    שמוקמור
    מור הו שמוק

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,11 +5,11 @@ public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+	//דימה לא חבר
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
