836943460649e4105de437ba3d0b4a85ab83cb0a
dimkatz <dimkatz89@gmail.com>
2015-07-09 18:50:44 +0300

    Revert "No Egyptian brackets!"
    This reverts commit 32820c1731c76b614e429cd97e08a721a2d89b12.

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -1,21 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour
-{
+public class TestScript : MonoBehaviour {
 
-    // Use this for initialization
-    void Start()
-    {
-        int j;
-        //wuzzzzzup
-        //דימה לא חבר
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //hello world!
-        //EITAN lets play hots!!!
-    }
+	// Use this for initialization
+	void Start () {
+		int j;
+		//wuzzzzzup
+	//דימה לא חבר
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//hello world!
+		//EITAN lets play hots!!!
+	}
 }
