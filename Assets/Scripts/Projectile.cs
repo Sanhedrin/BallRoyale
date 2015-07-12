9dec81a01c901bbe1bd375909bfa72dd28681409
Gezer <guydunski@gmail.com>
2015-07-12 15:45:56 +0300

    more...

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,7 +5,8 @@ public class Projectile : MonoBehaviour
 {
     private Rigidbody m_OtherRigidBody;
 
-    private GameObject m_Launcher;
+    [SerializeField]
+    float m_LifeTime = 5;
 
     [SerializeField]
     float m_ExplosionForce = 1000f;
@@ -13,17 +14,19 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     float m_ExplosionRadius = 1000f;
 
+    void Start()
+    {
+        Destroy(gameObject, m_LifeTime);
+    }
+
     void OnTriggerEnter(Collider i_Other)
     {
-        //Needs more work...
-        
-        //if (i_Other.tag == "Player")
-        //{
-        //    m_OtherRigidBody = i_Other.GetComponent<Rigidbody>();
+        if (i_Other.tag == "Player")
+        {
+            m_OtherRigidBody = i_Other.GetComponent<Rigidbody>();
 
-        //    m_OtherRigidBody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
-
-        //    Destroy(this);
-        //}
+            m_OtherRigidBody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+            Destroy(gameObject);
+        }
     }
 }
