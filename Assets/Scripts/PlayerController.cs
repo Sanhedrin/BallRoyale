82db6222ba59f7fde5ec0b18389a582eb13ab822
Paximillian <morzil@walla.com>
2015-07-13 15:51:38 +0300

    Partially implemented pushing power and various code rearrangement.
    -Changed the architecture of collision and movement management on the
    server to accomodate new pushing method based on damage.
    -Added health related values to player.
    -Rearranged layers and tags.
    -Rearranged PlayerController to be more readable.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,24 @@ using System.Collections;
 
 public partial class PlayerController : NetworkBehaviour
 {
+    [SyncVar]
     private bool m_Grounded = false;
-    
-    private Rigidbody m_RigidBody;
+
+    [SyncVar]
+    public int m_Health = 0;
+    private const int k_MaxHealth = 999;
+
+    /// <summary>
+    /// How the knockback should be calculated from the health. The lower this is, the higher the knockback scaling becomes.
+    /// </summary>
+    private const int k_HealthKnockbackStep = 100;
+
+    //Exposing this member will help us save performance by removing GetComponent() calls which are very expensive.
+    [HideInInspector]
+    public Rigidbody m_Rigidbody;
 
     // Instantiate a prefab with an attached Missile script
-    [SerializeField]
-    public Rigidbody m_ProjectileRigidBody;
+    public GameObject m_ProjectilePrefab;
 
     [SerializeField]
     private float m_MoveSpeed = 20;
@@ -24,89 +35,88 @@ public partial class PlayerController : NetworkBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        m_RigidBody = GetComponent<Rigidbody>();
+        m_Rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame, non-physics updates should be writen here.
     void Update()
     {
-        Vector3 velocityDir = m_RigidBody.velocity;
-
-        if (Input.GetButtonDown("Fire1") & Vector3.zero != velocityDir)
+        Vector3 velocityDir = m_Rigidbody.velocity;
+        
+        if (Input.GetButtonDown("Fire1") && Vector3.zero != velocityDir)
         {
-            Rigidbody clone;
-            clone = Instantiate(m_ProjectileRigidBody, transform.position + velocityDir.normalized, transform.rotation) as Rigidbody;
-            clone.velocity = velocityDir.normalized * m_ProjectileSpeed;
+            GameObject clone = Instantiate(m_ProjectilePrefab, transform.position + velocityDir.normalized, transform.rotation) as GameObject;
+            clone.GetComponent<Rigidbody>().velocity = velocityDir.normalized * m_ProjectileSpeed;
         }
     }
 
     // FixedUpdate is called before any physics calculation, this is where the physics code should go(as seen on the Unity tutorial)
     void FixedUpdate()
     {
-        if (!isLocalPlayer)
-            return;
+        if (isLocalPlayer)
+        {
+            //Query for the current state of relevent movement axes: (returns values from -1 to +1)
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            bool jump = Input.GetButtonDown("Jump") && m_Grounded;
 
-        //Query for the current state of relevent movement axes: (returns values from -1 to +1)
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        CmdMovementManagmnt(moveHorizontal, moveVertical);
+            CmdMovementManagement(moveHorizontal, moveVertical, jump);
+        }
     }
 
+    /// <summary>
+    /// Applies the movement on the server, which will then sync through the networkID to all the clients.
+    /// </summary>
+    /// <param name="i_Horizontal">Amount of horizontal movement.</param>
+    /// <param name="i_Vertical">Amount of vertical movement.</param>
+    /// <param name="i_Jump">Indicates if the player should jump.</param>
     [Command]
-    private void CmdMovementManagmnt(float i_Horizontal, float i_Vertical)
+    private void CmdMovementManagement(float i_Horizontal, float i_Vertical, bool i_Jump)
     {
         Vector3 movement = new Vector3(i_Horizontal, 0.0f, i_Vertical);
-        m_RigidBody.AddForce(movement * m_MoveSpeed * m_RigidBody.mass);
+        m_Rigidbody.AddForce(movement * m_MoveSpeed * m_Rigidbody.mass);
        
-        if (Input.GetButtonDown("Jump") && m_Grounded)
+        if (i_Jump)
         {
-            m_RigidBody.AddForce(Vector3.up * m_JumpSpeed);
+            m_Rigidbody.AddForce(Vector3.up * m_JumpSpeed * m_Rigidbody.mass);
         }
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    m_RigidBody.AddForce(Vector3.left * m_MoveSpeed);
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    m_RigidBody.AddForce(Vector3.right * m_MoveSpeed);
-        //}
-        //if (Input.GetKey(KeyCode.UpArrow))
-        //{
-        //    m_RigidBody.AddForce(Vector3.forward * m_MoveSpeed);
-        //}
-        //if (Input.GetKey(KeyCode.DownArrow))
-        //{
-        //    m_RigidBody.AddForce(Vector3.back * m_MoveSpeed);
-        //}
+    }
+    
+    /// <summary>
+    /// Increases the velocity of the pushed player by the amount of damage they've taken.
+    /// This is a coroutine and is only called on the server.
+    /// </summary>
+    /// <param name="i_PushedPlayer">Player to push</param>
+    private IEnumerator serverPushPlayer(GameObject i_PushedPlayer)
+    {
+        if (isServer)
+        {
+            yield return null;
+            yield return null;
+            PlayerController pushedPlayer = i_PushedPlayer.GetComponent<PlayerController>();
+            pushedPlayer.m_Rigidbody.velocity *= (1 + (float)pushedPlayer.m_Health / (float)PlayerController.k_HealthKnockbackStep);
+        }
     }
 
-    public void OnCollisionEnter(Collision i_TheCollision)
+    void OnCollisionEnter(Collision i_CollisionInfo)
     {
-        if (i_TheCollision.gameObject.CompareTag("Plain"))
+        if (i_CollisionInfo.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
             m_Grounded = true;
         }
 
-        // IN TESTING!
-        //if (i_TheCollision.gameObject.CompareTag("Player"))
-        //{
-        //    var otherPlayer = i_TheCollision.collider.gameObject;
-        //    var pushVector = i_TheCollision.gameObject.transform.position - m_RigidBody.transform.position * m_RigidBody.velocity.sqrMagnitude * 100;
-
-        //    CmdPushPlayer(otherPlayer, pushVector);
-        //}
+        if (i_CollisionInfo.gameObject.CompareTag("Player"))
+        {
+            if (isServer)
+            {
+                StartCoroutine(serverPushPlayer(i_CollisionInfo.collider.gameObject));
+            }
+        }
     }
 
-    [Command]
-    public void CmdPushPlayer(GameObject player, Vector3 force)
+    void OnCollisionExit(Collision i_CollisionInfo)
     {
-        player.GetComponent<Rigidbody>().AddForce(force);
-    }
-
-    public void OnCollisionExit(Collision i_TheCollision)
-    {
-        if (i_TheCollision.gameObject.CompareTag("Plain"))
+        if (i_CollisionInfo.gameObject.layer == LayerMask.NameToLayer("Floor"))
         {
             m_Grounded = false;
         }
