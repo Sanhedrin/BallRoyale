2f35fcdb23958a02f0f4118c1f76f66d5372eb5e
Paximillian <morzil@walla.com>
2015-07-15 21:14:23 +0300

    Separated player into 2 files. Need to separate the object pool once it's generic.

---
+++ Assets/Scripts/Player Scripts/PlayerControls.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+/// <summary>
+/// Manages player input and their effect.
+/// </summary>
+public partial class PlayerControls : NetworkBehaviour 
+{
+    [SyncVar]
+    private bool m_Grounded = false;
+
+    //Exposing this member will help us save performance by removing GetComponent() calls which are very expensive.
+    [HideInInspector]
+    public Rigidbody m_Rigidbody;
+
+    // Instantiate a prefab with an attached Missile script
+    [SerializeField]
+    private GameObject m_ProjectilePrefab;
+
+    [SerializeField]
+    private float m_MoveSpeed = 20;
+
+    [SerializeField]
+    private float m_JumpSpeed = 350;
+
+    [SerializeField]
+    private float m_ProjectileSpeed = 30;
+
+    const int k_ProjectilePooledAmount = 10;
+    List<GameObject> m_ProjectilePool;
+
+	// Use this for initialization
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        //Creating the pool of projectiles
+        m_ProjectilePool = new List<GameObject>();
+        for (int i = 0; i < k_ProjectilePooledAmount; i++)
+        {
+            GameObject obj = Instantiate(m_ProjectilePrefab);
+            obj.SetActive(false);
+            m_ProjectilePool.Add(obj);
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (isLocalPlayer)
+        {
+            //Query for the current state of relevent movement axes: (returns values from -1 to +1)
+            float moveHorizontal = Input.GetAxis(ConstNames.HorizontalAxis);
+            float moveVertical = Input.GetAxis(ConstNames.VerticalAxis);
+            bool jump = Input.GetButtonDown(ConstNames.JumpButton) && m_Grounded;
+
+            CmdMovementManagement(moveHorizontal, moveVertical, jump);
+        }
+    }
+
+	// Update is called once per frame
+    void Update()
+    {
+        if (isLocalPlayer && Input.GetButtonDown(ConstNames.FireButton))
+        {
+            Shoot();
+        }
+	}
+
+    /// <summary>
+    /// Applies the movement on the server, which will then sync through the networkID to all the clients.
+    /// </summary>
+    /// <param name="i_Horizontal">Amount of horizontal movement.</param>
+    /// <param name="i_Vertical">Amount of vertical movement.</param>
+    /// <param name="i_Jump">Indicates if the player should jump.</param>
+    [Command]
+    private void CmdMovementManagement(float i_Horizontal, float i_Vertical, bool i_Jump)
+    {
+        Vector3 movement = new Vector3(i_Horizontal, 0.0f, i_Vertical);
+        m_Rigidbody.AddForce(movement * m_MoveSpeed * m_Rigidbody.mass);
+
+        if (i_Jump)
+        {
+            m_Rigidbody.AddForce(Vector3.up * m_JumpSpeed * m_Rigidbody.mass);
+        }
+    }
+
+    void Shoot()
+    {
+        Vector3 velocityDir = m_Rigidbody.velocity.normalized;
+
+        if (Vector3.zero != velocityDir)
+        {
+            for (int i = 0; i < k_ProjectilePooledAmount; i++)
+            {
+                GameObject currObj = m_ProjectilePool[i];
+
+                if (!currObj.activeInHierarchy)
+                {
+                    currObj.transform.position = transform.position + velocityDir;
+                    currObj.transform.rotation = Quaternion.LookRotation(velocityDir);
+                    currObj.transform.Rotate(new Vector3(0, 90, 0)); // rotate the missle by 90 degrees on the y axes
+                    currObj.GetComponent<Rigidbody>().velocity = velocityDir * m_ProjectileSpeed;
+                    currObj.SetActive(true);
+                    break;
+                }
+            }
+        }
+    }
+
+    //Movement is completely done on the server and then sent to the clients after calculations, so there's no sense in
+    //checking for collision on the client, when the server already deals with it and nullifies client movement upon collision checks
+    [Server]
+    void OnCollisionEnter(Collision i_CollisionInfo)
+    {
+        if (i_CollisionInfo.gameObject.layer == LayerMask.NameToLayer(ConstNames.FloorLayer))
+        {
+            m_Grounded = true;
+        }
+    }
+
+    //Movement is completely done on the server and then sent to the clients after calculations, so there's no sense in
+    //checking for collision on the client, when the server already deals with it and nullifies client movement upon collision checks
+    void OnCollisionExit(Collision i_CollisionInfo)
+    {
+        if (i_CollisionInfo.gameObject.layer == LayerMask.NameToLayer(ConstNames.FloorLayer))
+        {
+            m_Grounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+using Assets.Scripts;
+
+/// <summary>
+/// The base script for the player will manage the display and stats of the player, as well as network synchronization.
+/// </summary>
+public class PlayerScript : NetworkBehaviour
+{
+    /// <summary>
+    /// The health of the player, make sure to only assign from server side.
+    /// </summary>
+    [SyncVar(hook="OnHealthChanged")]
+    private int m_Health = 0;
+    private void OnHealthChanged(int i_NewHealth)
+    {
+        m_Health = i_NewHealth < k_MaxHealth ? i_NewHealth : k_MaxHealth;
+    }
+
+    private const int k_MaxHealth = 999;
+
+    /// <summary>
+    /// How the knockback should be calculated from the health. The lower this is, the higher the knockback scaling becomes.
+    /// </summary>
+    private const int k_HealthKnockbackStep = 750;
+
+    //Exposing this member will help us save performance by removing GetComponent() calls which are very expensive.
+    [HideInInspector]
+    public Rigidbody m_Rigidbody;
+
+    private Text m_PlayerHealthText;
+
+    [SyncVar]
+    private int m_ConnectionID;
+
+    private delegate void IDAssignedEventHandler(object sender, IDEventArgs e);
+    private event IDAssignedEventHandler IDAssigned;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        IDAssigned += new IDAssignedEventHandler(PlayerController_IDAssigned);
+
+        if (isLocalPlayer)
+        {
+            CmdRequestID();
+        }
+        else
+        {
+            AssignTextForPlayer(m_ConnectionID);
+        }
+
+        Random.seed = System.DateTime.Now.Millisecond;
+	}
+	
+	// Update is called once per frame, non-physics updates should be writen here.
+    void Update()
+    {
+        if (m_PlayerHealthText)
+        {
+            m_PlayerHealthText.text = m_Health.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Requests the server to assign client an ID
+    /// </summary>
+    [Command]
+    private void CmdRequestID()
+    {
+        m_ConnectionID = NetworkManager.singleton.numPlayers;
+        RpcIDAssignResponse(m_ConnectionID);
+    }
+
+    /// <summary>
+    /// A response to the ID assignment request.
+    /// </summary>
+    /// <param name="i_IDAssignment">The ID this player was assigned</param>
+    [ClientRpc]
+    private void RpcIDAssignResponse(int i_IDAssignment)
+    {
+        OnIDAssigned(new IDEventArgs(i_IDAssignment));
+    }
+
+    /// <summary>
+    /// Raising the IDAssigned event.
+    /// </summary>
+    /// <param name="e">IDEventArgs containing the ID assigned to the player by the server.</param>
+    private void OnIDAssigned(IDEventArgs e)
+    {
+        if (IDAssigned != null)
+        {
+            IDAssigned.Invoke(this, e);
+        } 
+    }
+
+    //Activates all methods based on ID assignment
+    private void PlayerController_IDAssigned(object sender, IDEventArgs e)
+    {
+        AssignTextForPlayer(e.ID);
+    }
+
+    /// <summary>
+    /// Assigns the text object holding the health of a player to the corresponding player ID.
+    /// </summary>
+    private void AssignTextForPlayer(int i_PlayerID)
+    {
+        GameObject text = GameObject.Find(string.Format("Player{0}HP", i_PlayerID));
+        text.SetActive(true);
+        m_PlayerHealthText = text.GetComponent<Text>();
+    }
+
+    
+    /// <summary>
+    /// Increases the velocity of the pushed player by the amount of damage they've taken..
+    /// </summary>
+    /// <param name="i_PushedPlayer">Player to push</param>
+    [Server]
+    private IEnumerator serverPushPlayer(GameObject i_PushedPlayer)
+    {
+        if (isServer)
+        {
+            yield return null;
+            yield return null;
+            PlayerScript pushedPlayer = i_PushedPlayer.GetComponent<PlayerScript>();
+            pushedPlayer.m_Rigidbody.velocity *= (1 + (float)pushedPlayer.m_Health / (float)PlayerScript.k_HealthKnockbackStep);
+        }
+    }
+
+    /// <summary>
+    /// Once the player flies off the screen, this will handle respawning.
+    /// </summary>
+    [Server]
+    private void serverRespawnPlayer()
+    {
+        Transform spawnPoint = NetworkManager.singleton.startPositions[Random.Range(0, NetworkManager.singleton.startPositions.Count)];
+        transform.position = spawnPoint.position;
+
+        m_Health = 0;
+        serverInitializePhysicsInfo();
+    }
+
+    /// <summary>
+    /// Initializes physics for a respawned player.
+    /// </summary>
+    [Server]
+    private void serverInitializePhysicsInfo()
+    {
+        m_Rigidbody.velocity = Vector3.zero;
+    }
+
+    void OnCollisionEnter(Collision i_CollisionInfo)
+    {
+        if (isServer)
+        {
+            if (i_CollisionInfo.gameObject.layer == LayerMask.NameToLayer(ConstNames.KillBoxLayer))
+            {
+                serverRespawnPlayer();
+            }
+
+            if (i_CollisionInfo.gameObject.CompareTag(ConstNames.PlayerTag))
+            {
+                StartCoroutine(serverPushPlayer(i_CollisionInfo.collider.gameObject));
+            }
+        }
+    }
+}
