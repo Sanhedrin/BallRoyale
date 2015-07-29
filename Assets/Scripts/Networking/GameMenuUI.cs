695c150105f05a238aca4d18e246b673a866000c
Tzah Mama <aartist21@gmail.com>
2015-07-29 17:03:35 +0300

    Addedd working buttons to Game Menu
    GUI Soon™!

---
+++ Assets/Scripts/Networking/GameMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class GameMenuUI : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void StartHosting()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        NetworkManager.singleton.StartClient();
+    }
+}
