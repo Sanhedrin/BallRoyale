bfd8253ffbb80b20b7c8971e354e57a1c2b77b18
Paximillian <morzil@walla.com>
2015-08-09 18:34:41 +0300

    Fixed resolution, camera and health positioning on rectangle stage.

---
+++ Assets/Scripts/Editor/PlayerPrefsReset.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PlayerPrefsReset : EditorWindow {
+
+    [MenuItem("Edit/Reset Playerprefs")]
+    public static void DeletePlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+}
