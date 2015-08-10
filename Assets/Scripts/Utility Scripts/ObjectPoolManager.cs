29ad7a8601c15ab3f7613b6f3dc1b67b1582a351
eitanacton <eitanacton@gmail.com>
2015-08-10 16:52:00 +0300

    object pool

--- Assets/Scripts/Utility Scripts/ObjectPoolManager.cs
+++ Assets/Scripts/Utility Scripts/ObjectPoolManager.cs
@@ -8,11 +8,23 @@ public class ObjectPoolManager : MonoBehaviour
     public List<GameObject> ObjectsToPool = new List<GameObject>();
     public List<eObjectPoolNames> ObjectPoolNames = new List<eObjectPoolNames>();
     
-    private Dictionary<eObjectPoolNames, GameObjectPool> m_ObjectPoolDictionary = new Dictionary<eObjectPoolNames, GameObjectPool>();  
-
-    public ObjectPoolManager()
+    private Dictionary<eObjectPoolNames, GameObjectPool> m_ObjectPoolDictionary = new Dictionary<eObjectPoolNames, GameObjectPool>();
+   
+    void Start()
     {
-        
-    }   
+        if (GameObject.FindGameObjectsWithTag("ObjectPool").Length > 1)
+        {
+            Debug.LogError("Can't have more than one Object Pool Manager in a scene.");
+        }
+
+        foreach (eObjectPoolNames key in ObjectPoolNames)
+        {
+            m_ObjectPoolDictionary.Add(key, new GameObjectPool(ObjectsToPool[(int)key], 10));
+        }
+    }
+    public GameObjectPool GetPoolForObject(eObjectPoolNames i_ObjectName)
+    {
+        return m_ObjectPoolDictionary[i_ObjectName];
+    }
 	
 }
