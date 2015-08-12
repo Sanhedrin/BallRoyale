c32d7b7f6f4dcee06142fc6fba3de659710c2a78
eitanacton <eitanacton@gmail.com>
2015-08-12 14:16:50 +0300

    Obsticl

---
+++ Assets/Scripts/Object Scripts/LandMindScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class LandMindScript : Obstical {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
---
+++ Assets/Scripts/Object Scripts/Obstical.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+//[AddComponentMenu("BallGame Scripts/Object Scripts/Projectile")]
+public class Obstical : NetworkBehaviour
+{
+    private Rigidbody m_OtherRigidBody;
+    
+    private float m_LifeTime = 2f;
+
+    [SerializeField]
+    private float m_ExplosionForce = 1000f;
+
+    [SerializeField]
+    private float m_ExplosionRadius = 1000f;
+
+    void OnEnable()
+    {
+        if (isServer)
+        {
+            StartCoroutine(DestroyObsticl(m_LifeTime));
+        }
+    }
+
+    [Server]
+    IEnumerator DestroyObsticl(float i_StartIn)
+    {
+        yield return new WaitForSeconds(i_StartIn);
+        gameObject.SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
+    void OnTriggerEnter(Collider i_Other)
+    {
+        if (isServer)
+        {
+            if (i_Other.tag == ConstNames.PlayerTag)
+            {
+                m_OtherRigidBody = i_Other.GetComponent<Rigidbody>();
+
+                
+                m_OtherRigidBody.AddExplosionForce(m_ExplosionForce, transform.position, m_ExplosionRadius);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
